451dea489151c408f573dca11a49d30b94ec9555
n2rajo00 <128301054+n2rajo00@users.noreply.github.com>
2023-12-14 19:14:03 +0200

    Tämän hetkiset muutokset kaikki ja etusivun design

--- src/components/sitecomp/herosection.js
+++ src/components/sitecomp/herosection.js
@@ -4,8 +4,31 @@ export const HeroSection = () => {
   return (
     <div className="hero-section">
       <div className="hero-content">
-        <h1>Welcome to our Website</h1>
-        <p>Discover amazing things here</p>
+        <h1>Welcome to our Holly Christmas Shop</h1>
+        <p>It all began in the year 2023 when a group of students started</p>
+        <p>wondering what it would be like to start a business together.</p>
+        <br/>
+        <p>This journey has been exciting with many obstacles and those have now been turned into</p>
+        <p>this beautiful Christmas Shop where you can adventure and find your inner Christmas elf.</p>
+        <br/>
+        <br/>
+        <br/>
+        <p>Enjoy and have a Merry Christmas!</p>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
       </div>
     </div>
   );
