3d250efaad8a040f413c075196dfd6eee77f7546
kassu-me <kasim.meghjee@gmail.com>
2023-12-17 22:04:29 +0200

    fixed routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthorizationExample from './components/AuthorizationExample';
 import NavigationBar from './components/sitecomp/navbar';
 import { HeroSection } from './components/sitecomp/herosection';
@@ -10,30 +10,25 @@ import { Categories } from './components/categories';
 import CardCollection from './components/sitecomp/cardsection';
 import Special from './components/special';
 
-
-
-/**
- * App contains only routing links to examples.
- * Routing is built in index.js
- */
 function App() {
-
   return (
-    <div className='App'>
-      
-      <NavigationBar/>
+    
+      <div className='App'>
+        <NavigationBar />
         <Routes>
-          <Route path='/' element={<HeroSection/>}/>
-          <Route path='/auth' element={<AuthorizationExample/>}/>
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/products/:category' element={<CardCollection/>}/>
-          <Route path='/products' element={<CardCollection/>}/>
-          <Route path='/categories' element={<Categories/>}/>
-          <Route path='/special' element={<Special/>}/>
+          <Route path='/' element={<HeroSection />} />
+          <Route path='/auth' element={<AuthorizationExample />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/products/:category' element={<Products />} />
+          <Route path='/products' element={<CardCollection />} />
+          <Route path='/categories' element={<Categories />} />
+          <Route path='/special' element={<Special />} />
         </Routes>
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    
   );
 }
 
 export default App;
+
